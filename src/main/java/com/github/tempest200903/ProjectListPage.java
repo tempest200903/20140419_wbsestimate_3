ac328a5560cf430a851fd676516cdd58b36c6d7d
tempest200903 <tempest200903@gmail.com>
2014-06-08 18:54:23 +0900

    deleteProject

--- src/main/java/com/github/tempest200903/ProjectListPage.java
+++ src/main/java/com/github/tempest200903/ProjectListPage.java
@@ -20,7 +20,9 @@ import com.googlecode.mjorm.MongoDaoImpl;
 import com.googlecode.mjorm.ObjectIterator;
 import com.googlecode.mjorm.annotations.AnnotationsDescriptorObjectMapper;
 import com.googlecode.mjorm.query.DaoQuery;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
 
 public class ProjectListPage extends WebPage {
@@ -38,11 +40,32 @@ public class ProjectListPage extends WebPage {
 					+ String.valueOf(System.currentTimeMillis());
 			ProjectModel p = new ProjectModel(name);
 			projectModelList.add(Model.of(p));
-			save();
+			saveProject();
 		}
 	}
 
+	private final class DeleteProjectLink extends Link<String> {
+
+		private final Item<ProjectModel> item;
+
+		private static final long serialVersionUID = 1L;
+
+		private DeleteProjectLink(String id, Item<ProjectModel> item) {
+			super(id);
+			this.item = item;
+		}
+
+		@Override
+		public void onClick() {
+			IModel<ProjectModel> model = item.getModel();
+			projectModelList.remove(model);
+			deleteProject(model.getObject().getId());
+		}
+
+	}
+
 	private final class ProjectListView extends RefreshingView<ProjectModel> {
+
 		private static final long serialVersionUID = 1L;
 
 		private ProjectListView(String id) {
@@ -60,16 +83,8 @@ public class ProjectListPage extends WebPage {
 					item.getModel(), "name");
 			item.add(new Label("projectName", model));
 
-			Link<String> deleteProjectLink = new Link<String>("deleteProject") {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public void onClick() {
-					IModel<ProjectModel> model = item.getModel();
-					projectModelList.remove(model);
-					save();
-				}
-			};
+			Link<String> deleteProjectLink = new DeleteProjectLink(
+					"deleteProject", item);
 			item.add(deleteProjectLink);
 		}
 	}
@@ -89,21 +104,15 @@ public class ProjectListPage extends WebPage {
 		super(parameters);
 
 		setupMongo();
+		loadProject();
 
-		load();
-		if (false) {
-			projectModelList
-					.add(Model.of(new ProjectModel("sample project 1")));
-			projectModelList
-					.add(Model.of(new ProjectModel("sample project 2")));
-		}
 		add(new ProjectListView("projectList"));
 
 		Link<String> createProjectLink = new CreateProjectLink("createProject");
 		add(createProjectLink);
 	}
 
-	private void load() {
+	private void loadProject() {
 		System.out.println("load begin");
 		DaoQuery query = dao.createQuery();
 		query.setCollection(projectCollection);
@@ -116,7 +125,20 @@ public class ProjectListPage extends WebPage {
 		System.out.println("load end");
 	}
 
-	private void save() {
+	private void deleteProject(String id) {
+		System.out.println("deleteProject begin");
+		if (false) {
+			BasicDBObject document = new BasicDBObject();
+			document.put("id", id);
+			DB db = mongo.getDB("mydb");
+			DBCollection collection = db.getCollection(projectCollection);
+			collection.remove(document);
+		}
+		dao.deleteObject(projectCollection, id);
+		System.out.println("deleteProject end");
+	}
+
+	private void saveProject() {
 		System.out.println("save begin");
 		int size = projectModelList.size();
 		for (int i = 0; i < size; i++) {
