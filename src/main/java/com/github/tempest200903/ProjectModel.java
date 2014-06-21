798e24ab827967b9367cf41cb98599d4138e9190
tempest200903 <tempest200903@gmail.com>
2014-06-21 22:29:02 +0900

    ProjectModel にフィールド name を追加する。

--- src/main/java/com/github/tempest200903/ProjectModel.java
+++ src/main/java/com/github/tempest200903/ProjectModel.java
@@ -1,4 +1,18 @@
 package com.github.tempest200903;
 
+/**
+ *
+ */
 public class ProjectModel {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
