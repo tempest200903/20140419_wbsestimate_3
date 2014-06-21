eef631c132c2caa792e62934b685415cbf162031
tempest200903 <tempest200903@gmail.com>
2014-06-21 21:20:04 +0900

    organize imports

--- src/main/java/com/github/tempest200903/HomePage.java
+++ src/main/java/com/github/tempest200903/HomePage.java
@@ -1,8 +1,8 @@
 package com.github.tempest200903;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/github/tempest200903/ProjectList.java
+++ src/main/java/com/github/tempest200903/ProjectList.java
@@ -1,18 +1,13 @@
 package com.github.tempest200903;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import com.mongodb.Mongo;
+
 public class ProjectList extends WebPage {
 
 	private static final long serialVersionUID = 1L;
