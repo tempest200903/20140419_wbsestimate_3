2628574ef275dd3c277ec5147467b9b2f56c8d83
tempest200903 <tempest200903@gmail.com>
2014-06-21 21:46:19 +0900

    リファクタリング ProjectList を TopPage に変更

--- src/main/java/com/github/tempest200903/TopPage.java
+++ src/main/java/com/github/tempest200903/TopPage.java
@@ -8,13 +8,13 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-public class ProjectList extends WebPage {
+public class TopPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
 	List<ProjectModel> projectModelList = new ArrayList<ProjectModel>();
 
-	public ProjectList(final PageParameters parameters) {
+	public TopPage(final PageParameters parameters) {
 		super(parameters);
 
 		{
