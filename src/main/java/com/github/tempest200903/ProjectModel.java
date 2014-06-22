899f700a74c0f4282bf9277e54435348d036d80c
tempest200903 <tempest200903@gmail.com>
2014-06-22 18:52:54 +0900

    TopPageTest new する時に projectModelList を表示する。

--- src/main/java/com/github/tempest200903/ProjectModel.java
+++ src/main/java/com/github/tempest200903/ProjectModel.java
@@ -1,5 +1,7 @@
 package com.github.tempest200903;
 
+import java.io.Serializable;
+
 import org.bson.types.ObjectId;
 
 import com.google.code.morphia.annotations.Entity;
@@ -9,12 +11,15 @@ import com.google.code.morphia.annotations.Id;
  *
  */
 @Entity
-public class ProjectModel {
+public class ProjectModel implements Serializable {
 
 	@Id
 	ObjectId oid;
 
 	String title;
+
+	public ProjectModel() {
+	}
 
 	public String getTitle() {
 		return title;
--- src/main/java/com/github/tempest200903/TopPage.java
+++ src/main/java/com/github/tempest200903/TopPage.java
@@ -2,12 +2,18 @@ package com.github.tempest200903;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.google.code.morphia.Datastore;
@@ -17,6 +23,35 @@ import com.mongodb.Mongo;
 
 public class TopPage extends WebPage {
 
+	private final class ProjectListView extends RefreshingView<ProjectModel> {
+
+		private static final long serialVersionUID = 1L;
+
+		private ProjectListView(String id) {
+			super(id);
+		}
+
+		@Override
+		protected Iterator<IModel<ProjectModel>> getItemModels() {
+			return wicketProjectModelList.iterator();
+		}
+
+		@Override
+		protected void populateItem(final Item<ProjectModel> item) {
+			PropertyModel<ProjectModel> model = new PropertyModel<ProjectModel>(
+					item.getModel(), "title");
+			item.add(new Label("projectTitle", model));
+
+			// Link<String> deleteProjectLink = new DeleteProjectLink(
+			// "deleteProject", item);
+			// item.add(deleteProjectLink);
+			//
+			// Link<String> readProjectLink = new ReadProjectLink("readProject",
+			// item);
+			// item.add(readProjectLink);
+		}
+	}
+
 	private static final Logger myLogger = Logger.getLogger(TopPage.class
 			.getName());
 
@@ -24,15 +59,18 @@ public class TopPage extends WebPage {
 
 	List<ProjectModel> projectModelList = new ArrayList<ProjectModel>();
 
+	final List<IModel<ProjectModel>> wicketProjectModelList = new ArrayList<IModel<ProjectModel>>();
+
 	private transient Datastore datastore;
 
 	public TopPage(final PageParameters parameters) throws UnknownHostException {
 		super(parameters);
 
-		{
+		if (false) {
 			Label projectList = new Label("projectList");
 			add(projectList);
 		}
+		add(new ProjectListView("projectList"));
 
 		{
 			Link<String> createProject = new Link<String>("createProject") {
@@ -41,6 +79,9 @@ public class TopPage extends WebPage {
 				@Override
 				public void onClick() {
 					ProjectModel projectModel = new ProjectModel();
+					String title = "project unknown "
+							+ (System.currentTimeMillis() % 10000);
+					projectModel.setTitle(title);
 					projectModelList.add(projectModel);
 					myLogger.info("projectModelList.size() =: "
 							+ projectModelList.size());
@@ -60,6 +101,11 @@ public class TopPage extends WebPage {
 	void load() {
 		Query<ProjectModel> query = datastore.find(ProjectModel.class);
 		projectModelList = new ArrayList<ProjectModel>(query.asList());
+		for (ProjectModel projectModel : projectModelList) {
+			Model<ProjectModel> model = org.apache.wicket.model.Model
+					.of(projectModel);
+			wicketProjectModelList.add(model);
+		}
 	}
 
 	void save() {
